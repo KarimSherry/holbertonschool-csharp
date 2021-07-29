1e2a11d413255ace7b9f2f12570ede3e50eaf413
KarimSherry <karimedusherry@outlook.com>
2021-07-29 20:34:35 +0300

    Commit

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,12 +4,28 @@ namespace _0_print_array
 {
     class Array
     {
-        public static int[] CreatePrint(int size)
-        {
-            Console.WriteLine("Enter size");
-            size = Console.ReadLine();
-            for(int i=0;i<size;i++)
-            return i;
+             public static int[] CreatePrint(int size)
+             {
+                 if (size < 0)
+                 {
+                     Console.WriteLine("Size cannot be negative");
+                     return null;
+                 }
+                 else if (size == 0)
+                 Console.WriteLine(" ");
+                 else
+                 {
+                    int[] array = new int[size];
+                    for(int i=0;i<size;i++)
+                    {
+                        array[i] = i;
+                        Console.Write($"{i} ");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Array Length =" + size);
+                    }
+                    return array;
+        
+            }
         }
     }
 }
