e1e756e10b586771b5ef7017f466f87fcf45a79f
KarimSherry <karimedusherry@outlook.com>
2021-07-29 20:25:05 +0300

    Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0_print_array.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            Console.WriteLine("Enter size");
+            size = Console.ReadLine();
+            for(int i=0;i<size;i++)
+            return i;
+        }
+    }
+}
