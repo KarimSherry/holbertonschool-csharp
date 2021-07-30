65e2b4793368a1c45fe5ec3b48c971a18e170d03
KarimSherry <karimedusherry@outlook.com>
2021-07-31 01:51:56 +0300

    commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+    {
+        public static void Reverse(int[] array)
+        {
+            for (var i=array.Length;i>-1;i--)
+            Console.WriteLine(array[i]);
+        }
+    }
