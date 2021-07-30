438485ef36e75272944983dfd82f284778480d15
KarimSherry <karimedusherry@outlook.com>
2021-07-31 02:03:42 +0300

    commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    class Program
+    {
+        public static List<int> CreatePrint(int size)
+        {
+            if(size<0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            else
+            {
+                var list = new list<int>(size);
+                for(var i=0;i<list.Length;i++)
+                {
+                    list[i]=i;
+                }
+                return list;
+            }
+        }
+    }
