a63e88c1631d18123c34dd896508a8b41410e371
KarimSherry <karimedusherry@outlook.com>
2021-07-31 01:12:35 +0300

    commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+    class Array
+    {
+        public static int elementAt(int[] array, int index)
+        {
+            if (index < 0)
+            Console.Writeline("index is out of range");
+            else
+            {
+                int[] array = new int[index];
+            }
+        }
+    }
