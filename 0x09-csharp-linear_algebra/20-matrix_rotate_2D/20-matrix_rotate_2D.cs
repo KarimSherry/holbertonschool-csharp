aa42ab41256aaeeaffbaf3d776f84eb8de6bf440
KarimSherry <karimedusherry@outlook.com>
2021-08-27 22:47:53 +0300

    commit

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,41 +1,51 @@
 ﻿using System;
-
 /// <summary>
-/// A class reguarding matrices
+/// new class matrixmath
 /// </summary>
 class MatrixMath
 {
     /// <summary>
-    /// Rotates a square 2D matrix by a given angle in radians
+    /// method to rotate matrix by angle
     /// </summary>
-    /// <param name="matrix">The matrix to rotate</param>
-    /// <param name="angle">The angle in which to rotate by</param>
-    /// <returns>The resulting matrix after rotating</returns>
+    /// <param name="matrix"></param>
+    /// <param name="angle"></param>
+    /// <returns>new matrix</returns>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        double[,] fail = { { -1 } };
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            double[,] mat = new double[2, 2];
+            double cos = Math.Cos(angle);
+            double sin = Math.Sin(angle);
+            double[,] rot = new double[,]{
+                {cos,sin},
+                {-sin,cos}
+            };
+            int row = matrix.GetLength(0);
+            int col1 = matrix.GetLength(1);
+            int col2 = rot.GetLength(1);
+            double[,] mult = new double[row, col2];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col2; j++)
+                {
+                    double count = 0;
+                    for (int k = 0; k < col1; k++)
+                    {
+                        count = Math.Round(count + matrix[i, k] * rot[k, j], 2);
 
-        if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
-            return (fail);
+                    }
+                    mult[i, j] = count;
+                }
+            }
+            return mult;
+        }
 
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
-            return (fail);
+        else
+        {
+            double[,] min1 = { { -1 } };
+            return min1;
+        }
 
-        double[,] total = new double[2, 2];
-
-        double x1 = matrix[0, 0];
-        double y1 = matrix[0, 1];
-        double x2 = matrix[1, 0];
-        double y2 = matrix[1, 1];
-
-        double cos = Math.Cos(angle);
-        double sin = Math.Sin(angle);
-
-        total[0, 0] = Math.Round((x1 * cos), 2) - Math.Round((y1 * sin), 2);
-        total[0, 1] = Math.Round((x1 * sin), 2) + Math.Round((y1 * cos), 2);
-        total[1, 0] = Math.Round((x2 * cos), 2) - Math.Round((y2 * sin), 2);
-        total[1, 1] = Math.Round((x2 * sin), 2) + Math.Round((y2 * cos), 2);
-
-        return (total);
     }
 }
